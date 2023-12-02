90d5893cc6c513248fb237f6774c4fb9b349692f
Wooyoung Song <woosong@umich.edu>
2023-12-01 22:39:27 -0500

    please god help us and some predict function

--- main.cpp
+++ main.cpp
@@ -31,10 +31,67 @@ class Classifier {
 		}
 		vocabulary = word_posts.size();
 	}
-	void predict(const csvstream &test_data){
 
+	void predict(csvstream &test_data){
+		int total_correct = 0; //correcly predicted
+		int total = 0; //total data given
+		cout << "trained on " << total_posts << " examples" << '\n';
+		cout << "test data:" << '\n';
+
+		map<string, double> label_scores; //This hold the label and their long scrores
+
+		map<string, string> row;
+		while (test_data >> row) {
+			map<string, int>::iterator it = label_posts.begin(); //loops through all the labels that needs log-probability score 
+
+			string label = row["tag"];
+			set<string> post_words = unique_words(row["content"]);
+
+			while(it != label_posts.end()) {
+				string predictLabel = it->first;
+				double logPredict = log(static_cast<double>(it->second)/total_posts); //initial lnP(C)
+
+				for(const string &word : post_words) {
+					logPredict += makePredic(word, predictLabel);
+				}
+				label_scores.insert(make_pair(predictLabel, logPredict));
+				it++;
+			}
+			map<string, double>::iterator it1 = label_scores.begin();
+			double max_value = it1->second;
+			string nameLabel = it1->first;
+			for(; it1 != label_scores.end(); ++it1) {
+  			 	if (it1 ->second > max_value) {
+   			    	nameLabel = it1->first;
+    		   		max_value = it1->second;
+    			}
+			}
+			cout << " correct = " << label << ", " << "predicted = " << nameLabel << ", " << "log-probability score =" << max_value << '\n';
+			cout << " content = " << row["content"] << '\n';
+			if (label == nameLabel)
+				total_correct++;
+			total++;
+		}
+		cout << "performance: " << total_correct << " / " << total << " posts predicted correctly";
 	}
  private:
+	double makePredic(const std::string& word, const std::string& label){
+        std::pair<std::string, std::string> label_word_pair = std::make_pair(label, word);
+
+        if (label_word_posts.find(label_word_pair) != label_word_posts.end()) { //word found within the given label
+			map<pair<string, string>, int>::iterator it = label_word_posts.find(label_word_pair);
+			map<string, int>::iterator it1 = label_posts.find(label);
+            return log(static_cast<double>(it->second) / it1->second);
+
+        } else if(word_posts.find(word) != word_posts.end()){ //word found outside of the given label
+			map<pair<string, string>, int>::iterator it = label_word_posts.find(label_word_pair);
+            return log(static_cast<double>(it->second) / total_posts);
+
+		} else { //the word does not exist
+            return log(1.0 / (total_posts));
+        }
+	}
+
 	set<string> unique_words(const string &str) {
 		istringstream source(str);
 		set<string> words;
@@ -62,7 +119,7 @@ int main(int argc, char* argv[]) {
 
 	//error checking
   if (argc != 3) {
-	if (argc == 4 && argv[3] == "--debug") ;
+	if (argc == 4 && static_cast<string>(argv[3]) == "--debug") ;
 		else {
 			print_usage();
 			return 1;
